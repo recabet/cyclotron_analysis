04496a655827454753436bc77da94c6059a679dd
recabet <rajab.iskenderli@gmail.com>
2026-02-22 21:52:03 +0400

    run.py for easy running

---
+++ src/run.py
@@ -0,0 +1,11 @@
+from src.scripts import generate_training_data
+from src.scripts import train_super_resolution
+
+
+def main():
+    # generate_training_data.main()
+    train_super_resolution.main()
+
+
+if __name__ == "__main__":
+    main()
