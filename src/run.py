495b63a8a0a4a22a8b41d97f63ccbad3ef37ff6f
recabet <rajab.iskenderli@gmail.com>
2026-02-26 13:41:44 +0400

    new features in run

--- src/run.py
+++ src/run.py
@@ -1,10 +1,12 @@
 from src.scripts import generate_training_data
 from src.scripts import train_super_resolution
+from src.scripts import test_super_resolution
 
 
 def main():
-    generate_training_data.main()
-    train_super_resolution.main()
+    # generate_training_data.main()
+    # train_super_resolution.main()
+    test_super_resolution.main()
 
 
 if __name__ == "__main__":
